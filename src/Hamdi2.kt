ef981f63ed657b22b6852a68e1304131c684106b
student <abdullahihamdiabass@gmail.com>
2021-02-28 12:40:14 +0300

    assignment2

---
+++ src/Hamdi2.kt
@@ -0,0 +1,38 @@
+fun main() {
+    println("Hello hamdi")
+    division()
+    add()
+    println(true)
+
+
+
+
+
+}
+fun hamdi() {
+    println("Hello hamdi")
+}
+fun division() {
+    var num1=20
+    var num2=10
+    var sum=num1/num2
+    println(sum)
+
+}
+fun add() {
+    var num3=10
+    var num4=5
+    var num5=20
+    var num6=15
+    var sum=num3+num4+num5+num6
+    println(sum)
+
+}
+fun me() {
+    var me="hamdi is short"
+    println(true)
+
+}
+
+
+
